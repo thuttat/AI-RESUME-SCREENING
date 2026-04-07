57f9973fe3fc5a5c15af9f5f03a862c17dd519cc
Trịnh Thị Anh Thư <anhthu.ttath@gmail.com>
2026-04-07 19:59:23 +0700

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -5,10 +5,8 @@ const api = axios.create({
     timeout: 10000, 
 });
 
-// Interceptor cho Request: Đính kèm token vào Header
 api.interceptors.request.use(
     (config) => {
-        // SỬA: Lấy đúng "accessToken"
         const token = localStorage.getItem('accessToken');
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
@@ -18,7 +16,6 @@ api.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-// Interceptor cho Response: Xử lý khi Token hết hạn (401)
 api.interceptors.response.use(
     (response) => response,
     (error) => {
